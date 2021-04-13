6b4cc4e26e74fbf3d7392c4bf91aebf967867900
Irfan Asrori <irfanasrori@students.amikom.ac.id>
2021-04-13 15:16:26 +0700

    responsi

---
+++ Karyawan.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman3364
+{
+    class Karyawan
+    {
+        public int NIK { get; set; }
+        public string nama { get; set; }
+        public string gajibulanan { get; set; }
+        public int no { get; set; }
+
+        public void bulanan()
+        {
+            Console.WriteLine("{0}\t{1} {2}\t\t{3}", no, NIK, nama, gajibulanan);
+        }
+
+        public void kenaikan()
+        {
+            Console.WriteLine("{0}\t{1} {2}\t\t{3}", no, NIK, nama, gajibulanan);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman3364
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Karyawan paijo = new Karyawan();
+            Karyawan jono = new Karyawan();
+
+            paijo.nama = "Paijo";
+            paijo.NIK = 190302123;
+            paijo.gajibulanan = "3,000,000";
+            paijo.no = 1;
+
+            jono.nama = "Jono";
+            jono.NIK = 190302124;
+            jono.gajibulanan = "2,000,000";
+            jono.no = 2;
+
+            Console.WriteLine("No\tNIK/Nama\t\tGajibulanan");
+            Console.WriteLine("---------------------------------------------");
+
+            paijo.bulanan();
+            jono.bulanan();
+            Console.WriteLine();
+            Console.WriteLine();
+
+            paijo.gajibulanan = "3,300,000";
+            jono.gajibulanan = "2,200,000";
+
+            Console.WriteLine("Alhamdulillah dapat kenaikan gaji 10%!!");
+            Console.WriteLine();
+            Console.WriteLine("No\tNIK/Nama\t\tGajibulanan");
+            Console.WriteLine("---------------------------------------------");
+            paijo.kenaikan();
+            jono.kenaikan();
+
+            Console.ReadKey();
+        }
+    }
+}
